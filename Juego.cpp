093493daad014327209f20c1efd8ef2a1312f38a
Gilberto-IsPa <gilberto.islas.palomino@gmail.com>
2025-01-28 15:59:43 -0700

    Initial commit

---
+++ Juego.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gilbe on 28/01/2025.
+//
+
+#include "Juego.h"
+
+namespace Juego {
+} // Juego
---
+++ Juego.h
@@ -0,0 +1,16 @@
+//
+// Created by gilbe on 28/01/2025.
+//
+
+#ifndef JUEGO_H
+#define JUEGO_H
+
+namespace Juego {
+
+class Juego {
+
+};
+
+} // Juego
+
+#endif //JUEGO_H
